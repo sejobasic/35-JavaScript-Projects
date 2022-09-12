30d1666437a85e515ed9e8c5afb133c53d9ce093
Sejo Basic <sejoob@gmail.com>
2022-09-11 20:20:40 -0700

    drawing-app setting up event listeners

--- drawing-app/script.js
+++ drawing-app/script.js
@@ -3,7 +3,31 @@ const context = canvas.getContext('2d')
 
 //init
 let size = 20
+let isPressed = false
 let color = 'black'
+let x
+let y
+
+canvas.addEventListener('mousedown', (e) => {
+  isPressed = true
+
+  // x and y positioning
+  x = e.offsetX
+  y = e.offsetY
+  
+  drawCircle()
+})
+
+// Cleanup event
+canvas.addEventListener('mouseup', (e) => {
+  isPressed = false
+
+  // x and y positioning
+  x = undefined
+  y = undefined
+  
+  drawCircle()
+})
 
 // Path function
 function drawCircle(x, y) {
@@ -21,6 +45,3 @@ function drawLine(x1, y1, x2, y2) {
   context.lineWidth = size
   context.stroke()
 }
-
-drawCircle(100, 200)
-drawLine(300, 300, 300, 500)
