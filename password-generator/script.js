829a975a41d60aff25b0b7aec3bc4f3b957c64f7
Sejo Basic <sejoob@gmail.com>
2022-10-03 20:19:26 -0700

    Password generator - Code cleanup - Copy to clipboard functionality

--- password-generator/script.js
+++ password-generator/script.js
@@ -1,32 +1,42 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+import { characters } from './characters.js'
 
 const passwordContainer = document.querySelector('.password-container')
 const buttonEl = document.querySelector('.btn')
 
+const firstPasswordEl = document.createElement('div')
+const clipboardEl = document.createElement('img')
 
-const firstPasswordEl = document.createElement('div');
+firstPasswordEl.classList.add('password')
+clipboardEl.classList.add('clipboard')
 
-firstPasswordEl.classList.add('password');
-
+clipboardEl.src = 'clipboard.svg'
+clipboardEl.alt = 'clipboard copy paste icon'
 
 // Render passwords on click
 buttonEl.addEventListener('click', () => {
-    let firstPassword = getRandomPassword()
-    
-    firstPasswordEl.textContent = firstPassword
-    
-    passwordContainer.appendChild(firstPasswordEl)
-    passwordContainer.appendChild(secondPasswordEl)
-    
+  let firstPassword = getRandomPassword()
+
+  firstPasswordEl.textContent = firstPassword
+
+  passwordContainer.appendChild(firstPasswordEl)
+  passwordContainer.appendChild(clipboardEl)
+})
+
+// Copy password to clipboard
+clipboardEl.addEventListener('click', () => {
+  const password = firstPasswordEl.textContent
+
+  if (navigator.clipboard) {
+    navigator.clipboard.writeText(password).then(() => {
+      alert('Password has been copied to clipboard')
+    })
+  } else {
+    console.log('Browser Not compatible')
+  }
 })
 
 // Generate random password
 function getRandomPassword() {
-  const shuffled = [...characters].sort(() => 0.5 - Math.random());
-  return shuffled.slice(0, 15).join('');
+  const shuffled = [...characters].sort(() => 0.5 - Math.random())
+  return shuffled.slice(0, 15).join('')
 }
-
-
-
-
