cc5fc5538532102d6c1a571e443789f0efdfadf8
Sejo Basic <sejoob@gmail.com>
2022-10-11 18:34:06 -0700

    unit-converter javascript conversion function complete

--- unit-converter/script.js
+++ unit-converter/script.js
@@ -7,7 +7,10 @@
 
 const inputEl = document.querySelector('.num-input')
 const btnEl = document.querySelector('.btn')
-const convertedEl = document.querySelector('.converted')
+
+const lengthEl = document.querySelector('.length-card')
+const volumeEl = document.querySelector('.volume-card')
+const massEl = document.querySelector('.mass-card')
 
 btnEl.addEventListener('click', () => {
   convertMetric()
@@ -17,7 +20,27 @@ btnEl.addEventListener('click', () => {
 function convertMetric() {
   console.log(inputEl.value)
 
-  convertedEl.textContent = `${inputEl.value} meters = 65.616 feet | ${inputEl.value} feet = 6.096 meters `
+  let meter = inputEl.value * 3.2808
+  let feet = inputEl.value / 3.2808
+
+  let liter = inputEl.value * 0.264172
+  let gallon = inputEl.value / 0.264172
+
+  let kilo = inputEl.value * 2.20462
+  let pound = inputEl.value / 2.20462
+
+  meter = meter.toFixed(3)
+  feet = feet.toFixed(3)
+  liter = liter.toFixed(3)
+  gallon = gallon.toFixed(3)
+  kilo = kilo.toFixed(3)
+  pound = pound.toFixed(3)
+
+  
+
+  lengthEl.textContent = `${inputEl.value} meters = ${meter} feet | ${inputEl.value} feet = ${feet} meters`
+  volumeEl.textContent = `${inputEl.value} liters = ${liter} gallons | ${inputEl.value} gallons = ${gallon} liters`
+  massEl.textContent = `${inputEl.value} kilos = ${kilo} pounds | ${inputEl.value} pounds = ${pound} kilos`
 }
 
 
