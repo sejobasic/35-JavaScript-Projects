3bedf70ec6cc657b3958cee5f387a35d8093f77e
Sejo Basic <sejoob@gmail.com>
2022-10-27 16:29:09 -0700

    shopping list styling and basic functionality

--- shopping-list/script.js
+++ shopping-list/script.js
@@ -1,8 +1,25 @@
-const search = document.querySelector('.search')
-const btn = document.querySelector('.btn')
-const input = document.querySelector('.input')
+const addItemBtn = document.querySelector('.add-item-btn')
+const itemInput = document.querySelector('.item-input')
+const list = document.querySelector('.list')
 
-btn.addEventListener('click', () => {
-  search.classList.toggle('active')
-  input.focus()
+const shoppingList = []
+
+addItemBtn.addEventListener('click', function () {
+  if (shoppingList.includes(itemInput.value)) {
+    console.log('no duplicates')
+  } else {
+    shoppingList.push(itemInput.value)
+    render()
+  }
+  itemInput.value = ''
 })
+
+function render() {
+  let html = ''
+  for (let item of shoppingList) {
+    html += `<li class="list-item">${item}</li>`
+  }
+  list.innerHTML = html
+}
+
+render()
