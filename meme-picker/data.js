94f8cf31affb7e625f05f24f063280787eeb3dbb
Sejo Basic <sejoob@gmail.com>
2022-10-31 15:05:35 -0700

    F1 Meme data collection

---
+++ meme-picker/data.js
@@ -0,0 +1,115 @@
+export const memeData = [
+  {
+    emotionTags: ["funny"],
+    isGif: false,
+    image: "1682c569-1d54-4eb2-a2d8-917720c1379c.jpeg",
+    alt: "An f1 meme",
+},
+{
+    emotionTags: ["funny", "angry"],
+    isGif: false,
+    image: "Car Throttle.jpeg",
+    alt: "An f1 meme",
+},
+{
+    emotionTags: ["funny"],
+    isGif: false,
+    image: "cdd1efaa-c040-4da0-815b-d2b06745b68c.jpeg",
+    alt: "An f1 meme",
+},
+{
+    emotionTags: ["confused", "sad"],
+    isGif: false,
+    image: "confused2.jpeg",
+    alt: "An f1 meme",},
+{
+    emotionTags: ["dominant", "funny"],
+    isGif: false,
+    image: "dom.jpeg",
+    alt: "An f1 meme",},
+{
+    emotionTags: ["happy", "funny"],
+    isGif: false,
+    image: "F1 Memes.jpeg",
+    alt: "An f1 meme",},
+{
+    emotionTags: ["dominant"],
+    isGif: false,
+    image: "lewis2.jpeg",
+    alt: "An f1 meme",},
+{
+    emotionTags: ["sad"],
+    isGif: false,
+    image: "sad3.jpeg",
+    alt: "An f1 meme",},
+{
+    emotionTags: ["sad"],
+    isGif: false,
+    image: "sad2.jpeg",
+    alt: "An f1 meme",},
+{
+    emotionTags: ["dominant"],
+    isGif: false,
+    image: "nervous.jpeg",
+    alt: "An f1 meme",},
+{
+    emotionTags: ["sad"],
+    isGif: false,
+    image: "sad.jpeg",
+    alt: "An f1 meme",},
+{
+    emotionTags: ["sad", "funny"],
+    isGif: false,
+    image: "sebastian-vettel-fans_ Photo.jpeg",
+    alt: "An f1 meme",},
+{
+    emotionTags: ["funny"],
+    isGif: true,
+    image: "funny.gif",
+    alt: "An f1 meme",},
+{
+    emotionTags: ["funny"],
+    isGif: true,
+    image: "funny2.gif",
+    alt: "An f1 meme",},
+{
+    emotionTags: ["confused"],
+    isGif: true,
+    image: "confused.gif",
+    alt: "An f1 meme",},
+{
+    emotionTags: ["dominant"],
+    isGif: true,
+    image: "lewis.gif",
+    alt: "An f1 meme",},
+{
+    emotionTags: ["happy"],
+    isGif: true,
+    image: "happy.gif",
+    alt: "An f1 meme",},
+{
+    emotionTags: ["funny", "happy"],
+    isGif: true,
+    image: "funny3.gif",
+    alt: "An f1 meme",},
+{
+    emotionTags: ["happy", "dominant"],
+    isGif: true,
+    image: "lol.gif",
+    alt: "An f1 meme",},
+{
+    emotionTags: ["happy", "dominant"],
+    isGif: true,
+    image: "happy2.gif",
+    alt: "An f1 meme",},
+{
+    emotionTags: ["funny"],
+    isGif: true,
+    image: "fun.gif",
+    alt: "An f1 meme",},
+{
+    emotionTags: ["happy"],
+    isGif: true,
+    image: "seb.gif",
+    alt: "An f1 meme",},
+]
