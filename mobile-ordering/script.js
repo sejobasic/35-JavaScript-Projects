7d944defa059c085c778cd955da31f1758cab95e
Sejo Basic <sejoob@gmail.com>
2022-11-02 20:20:09 -0700

    Mobile Ordering App card details modal upon completion of order implemented

--- mobile-ordering/script.js
+++ mobile-ordering/script.js
@@ -3,19 +3,28 @@ import { menuArray } from './data.js'
 const menuSection = document.querySelector('.menu-section')
 const checkoutContainer = document.querySelector('.checkout-container')
 const totalPrice = document.querySelector('.total-price')
+const modal = document.querySelector('.modal-container')
+const orderBtn = document.querySelector('.complete')
 
 const orders = []
-// Listen for add and complete order buttons
+
+// Listen for add, remove and complete order buttons
 document.addEventListener('click', (e) => {
   if (e.target.dataset.add) {
     addItem(e.target.dataset.add)
   }
-  if (e.target.dataset.remove) {
+  else if (e.target.dataset.remove) {
     removeItem(e.target.dataset.remove)
+  }
+  else if (e.target.dataset.complete) {
+    completeOrder()
+  }
+  else if (e.target.dataset.close) {
+    closeModal()
   }
 })
 
-// Iterate through data and render main food items inside content container
+// Iterate through data and render main food items inside menu section
 function renderFoodItems() {
   let menuHtml = ``
 
@@ -43,6 +52,27 @@ function addItem(itemId) {
   })[0]
   orders.push(orderedItemObj)
 
+  orderBtn.removeAttribute('disabled')
+
+  renderCheckout()
+}
+
+// Calculate total cost of order
+function calculateTotal() {
+  let sum = 0
+  orders.forEach((order) => {
+    return (sum += order.price)
+  })
+  totalPrice.textContent = `$${sum}`
+}
+
+// Remove single item from order based on target id
+function removeItem(itemId) {
+  const targetOrder = orders.forEach((order) => {
+    return order.id.indexOf(itemId)
+  })
+  orders.splice(targetOrder, 1)
+
   renderCheckout()
 }
 
@@ -57,22 +87,25 @@ function renderCheckout() {
       <p class="item-price">$${order.price}</p>
     </div>`
   })
+  if (orders.length < 1) {
+    orderBtn.setAttribute('disabled', true)
+  }
   checkoutContainer.innerHTML = ordersHtml
   calculateTotal()
 }
 
-function calculateTotal() {
-  let sum = 0
-  orders.forEach((order) => {
-    return (sum += order.price)
-  })
-  totalPrice.textContent = `$${sum}`
+function completeOrder() {
+  // Only show modal if order is not empty
+
+  if (orders.length > 0) {
+    modal.style.display = 'block'
+  }
 }
 
-function removeItem(itemId) {
-  const targetOrder = orders.map((order) => order.id).indexOf(itemId)
-  orders.splice(targetOrder, 1)
-  renderCheckout()
+function closeModal() {
+  modal.style.display = 'none'
 }
+
+
 
 renderFoodItems()
