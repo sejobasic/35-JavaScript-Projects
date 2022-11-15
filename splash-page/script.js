6f7d20798163bd478b6c1ea2b3491cb435e1769c
Sejo Basic <sejoob@gmail.com>
2022-11-14 17:19:52 -0700

    Splash page event on submit complete

--- splash-page/script.js
+++ splash-page/script.js
@@ -1,2 +1,14 @@
 const formEl = document.querySelector('.signup-form')
-const mainContent = document.querySelector
+const mainContent = document.querySelector('.main-content')
+
+formEl.addEventListener('submit', () => {
+  mainContent.innerHTML = `
+    <div class='success-wrapper'>
+      <i class="fa-solid fa-check"></i>
+      <h2 class='success-text'>Thank you for signing up!</h2>
+      <p>You will receive special weekly deals from 
+        Freshly Ground directly to your inbox.
+      </p>
+    </div>
+  `
+})
