3fa766caa260f5c79178118d80a9b2c1c1a6a4c5
Sejo Basic <sejoob@gmail.com>
2022-11-19 15:16:59 -0700

    dice-game created helper functions for random dice roll array and rendering dice on card

--- dice-game/script.js
+++ dice-game/script.js
@@ -1,3 +1,17 @@
+function getDiceRollArray(diceCount) {
+  return new Array(diceCount).fill(0).map(function () {
+    return Math.floor(Math.random() * 6) + 1
+  })
+}
+
+function getDiceHtml(diceCount) {
+  return getDiceRollArray(diceCount)
+    .map(function (num) {
+      return `<div class="dice">${num}</div>`
+    })
+    .join('')
+}
+
 const hero = {
   elementId: 'hero',
   name: 'Wizard',
@@ -15,18 +29,18 @@ const monster = {
 }
 
 function renderCharacter(data) {
-  const { elementId, name, avatar, health, diceCount } = data;
+  const { elementId, name, avatar, health, diceCount } = data
+  const diceHtml = getDiceHtml(diceCount)
 
-  document.getElementsByClassName(elementId).innerHTML =
-      `<div class="character-card">
+  document.getElementById(elementId).innerHTML = `<div class="character-card">
           <h4 class="name"> ${name} </h4>
           <img class="avatar" src="${avatar}" />
           <div class="health">health: <b> ${health} </b></div>
           <div class="dice-container">    
               ${diceHtml}
           </div>
-      </div>`;
+      </div>`
 }
 
-renderCharacter(hero);
-renderCharacter(monster);
+renderCharacter(hero)
+renderCharacter(monster)
