6d6836263f544e7534bcd1a7fa50eb4a0f24c151
Sejo Basic <sejoob@gmail.com>
2022-11-19 14:08:35 -0700

    dice-game import image avatar and hard code character objects

---
+++ dice-game/script.js
@@ -0,0 +1,15 @@
+const hero = {
+  elementId: 'hero',
+  name: 'Wizard',
+  avatar: 'images/wizard.png',
+  health: 60,
+  diceCount: 3,
+}
+
+const monster = {
+  elementId: 'monster',
+  name: 'Orc',
+  avatar: 'images/orc.png',
+  health: 10,
+  diceCount: 1,
+}
