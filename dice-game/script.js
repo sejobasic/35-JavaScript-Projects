b68eb8f8bb7971627d7b7afce654893771f46d5c
Sejo Basic <sejoob@gmail.com>
2022-11-19 15:27:35 -0700

    dice-game convert render-character func to constructor and create new instances of characters

--- dice-game/script.js
+++ dice-game/script.js
@@ -28,19 +28,33 @@ const monster = {
   diceCount: 1,
 }
 
-function renderCharacter(data) {
-  const { elementId, name, avatar, health, diceCount } = data
-  const diceHtml = getDiceHtml(diceCount)
 
-  document.getElementById(elementId).innerHTML = `<div class="character-card">
+function Character(data) {
+  this.elementId = data.elementId;
+  this.name = data.name;
+  this.avatar = data.avatar;
+  this.health = data.health;
+  this.diceCount = data.diceCount;
+
+  this.getCharacterHtml = function () {
+      const { elementId, name, avatar, health, diceCount } = data;
+      const diceHtml = getDiceHtml(diceCount)
+
+      document.getElementById(elementId).innerHTML =
+          `<div class="character-card">
           <h4 class="name"> ${name} </h4>
           <img class="avatar" src="${avatar}" />
           <div class="health">health: <b> ${health} </b></div>
           <div class="dice-container">    
               ${diceHtml}
           </div>
-      </div>`
+      </div>`;
+
+  }
 }
 
-renderCharacter(hero)
-renderCharacter(monster)
+const wizard = new Character(hero)
+const orc = new Character(monster)
+
+wizard.getCharacterHtml()
+orc.getCharacterHtml()
