27086c2b1bc5c669a0c6011d0fb8413b8c323de2
Sejo Basic <sejoob@gmail.com>
2022-11-21 16:12:19 -0700

    dice-game add click event on attack button to trigger new dice roll for each character

--- dice-game/script.js
+++ dice-game/script.js
@@ -1,8 +1,16 @@
 import characterData from './data.js'
 import Character from './character.js'
+import { getDiceRollArray } from './utils.js'
 
 const wizard = new Character(characterData.hero)
 const orc = new Character(characterData.monster)
+
+document.getElementById('attack-button').addEventListener('click', attack)
+
+function attack() {
+  getDiceRollArray()
+  render()
+}
 
 function render() {
   document.getElementById(wizard.elementId).innerHTML =
