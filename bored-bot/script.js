d5c31e34b93bbc0cf1b6d64c7cac9c55eb70a10c
Sejo Basic <sejoob@gmail.com>
2022-11-26 18:32:06 -0700

    bored-bot code cleanup and reformat

--- bored-bot/script.js
+++ bored-bot/script.js
@@ -1,13 +1,13 @@
 function getData() {
   fetch('https://apis.scrimba.com/bored/api/activity')
-  .then(response => response.json())
-  .then(data => renderActivity(data))
+    .then((response) => response.json())
+    .then((data) => renderActivity(data))
 }
 
-
-
 function renderActivity(data) {
-  return document.querySelector('.activity-container').innerHTML = `<h2>${data.activity}</h2>`
+  return (document.querySelector(
+    '.activity-container'
+  ).innerHTML = `<h2>${data.activity}</h2>`)
 }
 
 document.querySelector('.activity-btn').addEventListener('click', getData)
