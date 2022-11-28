961a6f5bfe9092ea17de11bd81b80e54c7452d1a
Sejo Basic <sejoob@gmail.com>
2022-11-28 13:04:27 -0700

    color-palette-generator fetch color data based off url query and user inputed value on button click

---
+++ color-palette-generator/script.js
@@ -0,0 +1,49 @@
+const colorInput = document.querySelector('.color-input')
+const selectMode = document.querySelector('.select-mode')
+const colorContainer = document.querySelector('.color-container')
+const formEl = document.querySelector('.actions-container')
+const getColorBtn = document.querySelector('.get-color-btn')
+
+let colorsArr = []
+let selectedColor = colorInput.value.slice(1)
+let selectedMode = selectMode.value
+
+colorInput.addEventListener('change', getColor)
+selectMode.addEventListener('change', getMode)
+
+function getColor(e) {
+  selectedColor = e.target.value.slice(1)
+  return selectedColor
+}
+
+function getMode(e) {
+  selectedMode = e.target.value
+  return selectedMode
+}
+
+getColorBtn.addEventListener('click', function () {
+  console.log(selectedColor)
+  console.log(selectedMode)
+  fetch(
+    `https://www.thecolorapi.com/scheme?hex=${selectedColor}&mode=${selectedMode}`
+  )
+    .then((resp) => resp.json())
+    .then((data) => {
+      colorsArr = data.colors
+      renderColors()
+    })
+})
+
+function renderColors() {
+  let colorsHtml = ''
+  console.log(colorsHtml)
+
+  for (let color of colorsArr) {
+    colorsHtml += `
+        <div class='color-block' style='background-color:${color.hex.value}'></div>
+        <h3 class='hex-code'>${color.hex.value}</h3>
+      `
+  }
+
+  colorContainer.innerHTML = colorsHtml
+}
