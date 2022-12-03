e3e1e96fb509a6db046be153bb009ea7b76cb44e
Sejo Basic <sejoob@gmail.com>
2022-12-02 17:15:51 -0700

    watch-list search query function rendering movies based on imdbID

--- watch-list/script.js
+++ watch-list/script.js
@@ -1,7 +1,9 @@
 const searchBar = document.querySelector('.search-bar')
 const searchBtn = document.querySelector('.search-btn')
+const contentContainer = document.querySelector('.content-container')
 
 let searchValue = searchBar.value
+let movieId = []
 
 function getSearchValue(e) {
   searchValue = e.target.value
@@ -11,7 +13,43 @@ function getSearchValue(e) {
 searchBar.addEventListener('change', getSearchValue)
 
 searchBtn.addEventListener('click', () => {
-  fetch(`http://www.omdbapi.com/?t=${searchValue}&apikey=771896a`)
+  fetch(`http://www.omdbapi.com/?s=${searchValue}&apikey=771896a&`)
   .then(resp => resp.json())
-  .then(data => console.log(data))
+  .then(data => {
+    data.Search.map(movie => {
+    movieId.push(movie.imdbID)
+  })
+  renderMovie()
+}
+  )
 })
+
+function renderMovie() {
+  
+  movieId.map(id => {
+    fetch(`http://www.omdbapi.com/?i=${id}&apikey=771896a`)
+    .then(resp => resp.json())
+    .then(movie => 
+      contentContainer.innerHTML = `
+        <div class="movie-container">
+        <img
+          class="movie-poster"
+          src="${movie.Poster}"
+          alt="movie poster"
+        />
+        <div class="movie-info">
+          <h3 class="movie-title">${movie.Title}</h3>
+          <div class="movie-actions">
+            <h4><img src="./images/rating.png" alt="star rating" /> ${movie.imdbRating}</h4>
+            <h4 class="movie-genre">${movie.Genre}</h4>
+            <button class="add-btn">
+              <img src="./images/add.png" alt="add button" />
+            </button>
+          </div>
+          <p class="movie-description">${movie.Plot}</p>
+        </div>
+      </div>
+        `
+      )
+  })
+}
