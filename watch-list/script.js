369b23b211f68dab3d532184a1d15a0b39312585
Sejo Basic <sejoob@gmail.com>
2022-12-03 17:03:06 -0700

    watch-list add individual movie to watch list

--- watch-list/script.js
+++ watch-list/script.js
@@ -1,9 +1,13 @@
 const searchBar = document.querySelector('.search-bar')
 const searchBtn = document.querySelector('.search-btn')
 const contentContainer = document.querySelector('.content-container')
+const listContainer = document.querySelector('.watchlist-container')
+
+listContainer.textContent = 'hello'
 
 let searchValue = searchBar.value
-let movieId = []
+let movieList = []
+let list = []
 
 function getSearchValue(e) {
   searchValue = e.target.value
@@ -12,44 +16,86 @@ function getSearchValue(e) {
 
 searchBar.addEventListener('change', getSearchValue)
 
-searchBtn.addEventListener('click', () => {
-  fetch(`http://www.omdbapi.com/?s=${searchValue}&apikey=771896a&`)
-  .then(resp => resp.json())
-  .then(data => {
-    data.Search.map(movie => {
-    movieId.push(movie.imdbID)
-  })
-  renderMovie()
-}
-  )
+document.addEventListener('click', (e) => {
+  if (e.target.dataset.add) {
+    addMovieToList(e.target.dataset.add)
+  }
 })
 
-function renderMovie() {
-  
-  movieId.map(id => {
-    fetch(`http://www.omdbapi.com/?i=${id}&apikey=771896a`)
-    .then(resp => resp.json())
-    .then(movie => 
-      contentContainer.innerHTML = `
-        <div class="movie-container">
-        <img
-          class="movie-poster"
-          src="${movie.Poster}"
-          alt="movie poster"
-        />
-        <div class="movie-info">
-          <h3 class="movie-title">${movie.Title}</h3>
-          <div class="movie-actions">
-            <h4><img src="./images/rating.png" alt="star rating" /> ${movie.imdbRating}</h4>
-            <h4 class="movie-genre">${movie.Genre}</h4>
-            <button class="add-btn">
-              <img src="./images/add.png" alt="add button" />
-            </button>
-          </div>
-          <p class="movie-description">${movie.Plot}</p>
+searchBtn.addEventListener('click', async () => {
+  const resp = await fetch(`http://www.omdbapi.com/?s=${searchValue}&apikey=771896a&`)
+  const data = await resp.json()
+
+  renderMovie(data.Search)
+})
+
+async function renderMovie(data) {
+  let movieHTML = ''
+
+  for (let id of data) {
+    const movieResp = await fetch(`http://www.omdbapi.com/?i=${id.imdbID}&apikey=771896a`)
+    const movieData = await movieResp.json()
+    movieList.push(movieData)
+
+
+
+    movieHTML += `
+    <div class="movie-container">
+      <img
+        class="movie-poster"
+        src="${movieData.Poster}"
+        alt="movie poster"
+      />
+      <div class="movie-info">
+        <h3 class="movie-title">${movieData.Title}</h3>
+        <div class="movie-actions">
+          <h4><img src="./images/rating.png" alt="star rating" /> ${movieData.imdbRating}</h4>
+          <h4 class="movie-genre">${movieData.Genre}</h4>
+          <button class="add-btn">
+            <img data-add="${movieData.imdbID}" src="./images/add.png" alt="add button" />
+          </button>
         </div>
+        <p class="movie-description">${movieData.Plot}</p>
       </div>
-        `
-      )
+    </div>
+    `
+  }
+  contentContainer.innerHTML = movieHTML
+}
+
+function addMovieToList(movieId) {
+
+  const targetObj = movieList.filter(movie => {
+    console.log(movie.imdbID)
+    return movie.imdbID === movieId
+  })[0]
+
+  list.push(targetObj)
+  console.log(list)
+  renderList()
+}
+
+function renderList() {
+  list.map(movie => {
+  contentContainer.innerHTML += `
+  <div class="movie-container">
+  <img
+    class="movie-poster"
+    src="${movie.Poster}"
+    alt="movie poster"
+  />
+  <div class="movie-info">
+    <h3 class="movie-title">${movie.Title}</h3>
+    <div class="movie-actions">
+      <h4><img src="./images/rating.png" alt="star rating" /> ${movie.imdbRating}</h4>
+      <h4 class="movie-genre">${movie.Genre}</h4>
+      <button class="add-btn">
+        <img data-add="${movie.imdbID}" src="./images/add.png" alt="add button" />
+      </button>
+    </div>
+    <p class="movie-description">${movie.Plot}</p>
+  </div>
+</div>
+  `
   })
 }
