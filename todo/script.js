14cbaa5335c5c590ec6e01b248fae5d34b359c93
Sejo Basic <sejoob@gmail.com>
2023-03-07 22:10:39 -0700

    My List - Add todo list item with click event populated by input value - completed

---
+++ todo/script.js
@@ -0,0 +1,32 @@
+const todoInput = document.querySelector('.todo-input')
+const todoBtn = document.querySelector('.todo-btn')
+const todoList = document.querySelector('.todo-list')
+
+todoBtn.addEventListener('click', addTodo)
+todoList.addEventListener('click', removeTodo)
+
+
+function addTodo(e) {
+  e.preventDefault()
+
+  todoList.innerHTML += `
+    <div class='todo'>
+      <li class='todo-item'>${todoInput.value}</li>
+      <button class='complete-btn'>
+        <i class='fas fa-check'></i>
+      </button>
+      <button class='remove-btn'>
+      <i class='fas fa-trash'></i>
+    </button>
+    </div>
+  `
+  todoInput.value = ''
+}
+
+function removeTodo(e) {
+  const item = e.target
+  console.log(item)
+  if (item.classList[0] === 'remove-btn') {
+    item.remove()
+  }
+}
