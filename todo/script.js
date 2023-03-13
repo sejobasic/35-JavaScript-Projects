3385bd90539887443e64cdd595fe4385f6f8757f
Sejo Basic <sejoob@gmail.com>
2023-03-12 19:36:23 -0700

    My List - Added styling for disabled button

--- todo/script.js
+++ todo/script.js
@@ -4,23 +4,37 @@ const todoList = document.querySelector('.todo-list')
 
 todoBtn.addEventListener('click', addTodo)
 todoList.addEventListener('click', removeTodo)
+todoInput.addEventListener('keyup', checkInput)
 
+function checkInput(e) {
+  if (e.target.value.length > 0) {
+    todoBtn.classList.remove('disabled')
+    todoBtn.disabled = false
+  } else {
+    todoBtn.classList.add('disabled')
+    todoBtn.disabled = true
+  }
+}
 
 function addTodo(e) {
   e.preventDefault()
 
-  todoList.innerHTML += `
-    <div class='todo'>
-      <li class='todo-item'>${todoInput.value}</li>
-      <button class='complete-btn'>
-        <i class='fas fa-check'></i>
+  if (todoInput.value) {
+    todoList.innerHTML += `
+      <div class='todo'>
+        <li class='todo-item'>${todoInput.value}</li>
+        <button class='complete-btn'>
+          <i class='fas fa-check'></i>
+        </button>
+        <button class='remove-btn'>
+        <i class='fas fa-trash'></i>
       </button>
-      <button class='remove-btn'>
-      <i class='fas fa-trash'></i>
-    </button>
-    </div>
-  `
-  todoInput.value = ''
+      </div>
+    `
+    todoInput.value = ''
+    todoBtn.classList.add('disabled')
+    todoBtn.disabled = true
+  }
 }
 
 function removeTodo(e) {
