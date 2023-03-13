edc948ffb9db1363e4a75d08ded7461f7a6a413e
Sejo Basic <sejoob@gmail.com>
2023-03-12 20:22:25 -0700

    My List - Filter tasks by completed or incomplete

--- todo/script.js
+++ todo/script.js
@@ -1,10 +1,12 @@
 const todoInput = document.querySelector('.todo-input')
 const todoBtn = document.querySelector('.todo-btn')
 const todoList = document.querySelector('.todo-list')
+const filterOption = document.querySelector('.filter-todo')
 
 todoBtn.addEventListener('click', addTodo)
 todoList.addEventListener('click', removeTodo)
 todoInput.addEventListener('keyup', checkInput)
+filterOption.addEventListener('click', filterTodo)
 
 function checkInput(e) {
   if (e.target.value.length > 0) {
@@ -42,6 +44,31 @@ function removeTodo(e) {
   console.log(item)
   if (item.classList[0] === 'remove-btn') {
     const todo = item.parentElement
-    todo.remove()
+    todo.classList.add('fall')
+    todo.addEventListener('transitionend', () => {
+      todo.remove()
+    })
   }
+
+  if (item.classList[0] === 'complete-btn') {
+    const todo = item.parentElement
+    todo.classList.toggle('completed')
+  }
+}
+
+function filterTodo(e) {
+  const todos = todoList.childNodes;
+  todos.forEach((todo) => {
+    switch (e.target.value) {
+      case 'all':
+        return todo.style.display = 'flex'
+
+      case 'completed':
+        if (todo.classList.contains('completed')) {
+          return todo.style.display = 'flex'
+        } else {
+          return todo.style.display = 'none'
+        }
+    }
+  })
 }
